bf30fc1c826ec72bfe28ef09978b7bfa5e183c45
Andre Issa <andrelouisissa@gmail.com>
2021-09-03 03:02:47 +1000

    table separation

--- mkmdtbl.py
+++ mkmdtbl.py
@@ -4,8 +4,8 @@ ROW = "\n</td></tr><tr><td>\n\t"
 ROWSEP = "\n</td></tr></table><table><tr><td>\n\t"
 COL = "\n</td><td>\n\t"
 COLSEP = "\n</td><td colspan=\"2\">\n\t"
-SEP = ">>> "
-SKIP = "----"
+SEP = "----"
+SKIP = ">>>>"
 SECTIONCHR = "$"
 ANCHORCHR = "#"
 ANCHOR = """<a id="user-content-{1}" href="#{1}" aria-hidden="true">{0}</a>"""
@@ -17,11 +17,14 @@ def get_anchor( s, section="" ):
     clean = re.sub(r"[^A-Za-z0-9\-]","",(section + s).replace(" ","-").lower( ))
     return ANCHOR.format(s,clean)
 
+def rep_sep( s ):
+    return s.replace("\n\n"+SEP+"\n",SEP+"\n")
+
 def rep_code( s ):
     return re.sub(r"`(.*?)`",r"<code>\1</code>",s)
 
 def table( txt ):
-    txt = rep_code( txt )
+    txt = rep_code(rep_sep(txt))
     t = list(map(lambda x: x.split("\n"), txt.split("\n\n")))
     section = ""
     sections = []
@@ -35,7 +38,7 @@ def table( txt ):
             r[0] = get_anchor(c[1:], section)
     for i in sorted(sections,reverse=True):
         del t[i]
-    return START+ROW.join(map(lambda r: COL.join(r).replace(SKIP,"").replace(COL+SEP,COLSEP), t)).replace(ROW+SEP,ROWSEP)+END
+    return START+ROW.join(map(lambda r: COL.join(r).replace(SKIP,"").replace(COL+SEP,COLSEP), t)).replace(SEP+ROW,ROWSEP)+END
     
 def printtable( filename ):
     with open(filename) as file:
